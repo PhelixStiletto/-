5dd0f181015b1bc9788dc02612807c3f019b000a
PhelixStiletto <ph9964035808@gmail.com>
2024-10-31 19:30:02 +0500

    Добавлена обработка исключений.

--- Main.py
+++ Main.py
@@ -1,18 +1,19 @@
 from opencage.geocoder import OpenCageGeocode
-from selenium.webdriver.support.expected_conditions import element_selection_state_to_be
-
 
 def get_coordinates(city,key):
-     geocoder = OpenCageGeocode(key)
-     query = city
-     results=geocoder.geocode(query)
-     if results:
-         return results[0]['geometry']['lat'],results[0]['geometry']['lng']
-     else:
-        return 'Город е найден.'
+    try:
+         geocoder = OpenCageGeocode(key)
+         query = city
+         results=geocoder.geocode(query)
+         if results:
+             return results[0]['geometry']['lat'],results[0]['geometry']['lng']
+         else:
+            return 'Город е найден.'
+    except Exception as e:
+        return  f'Возникла ошибка {e}'
 
 key = '82f3b2fd29a54869a709e582a85e84b7'
-city='Moscow'
+city='London'
 coordinates = get_coordinates(city,key)
 print(f"Координаты города {city}: {coordinates}")
 
