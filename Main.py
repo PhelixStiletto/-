84f56f81e454e80482db0f7db6c2ab0ede42d146
PhelixStiletto <ph9964035808@gmail.com>
2024-10-31 19:50:04 +0500

    Добавлено округление координат

--- Main.py
+++ Main.py
@@ -1,19 +1,21 @@
+from django.templatetags.i18n import language
 from opencage.geocoder import OpenCageGeocode
 
 def get_coordinates(city,key):
     try:
          geocoder = OpenCageGeocode(key)
-         query = city
-         results=geocoder.geocode(query)
+         results=geocoder.geocode(city,language='ru')
          if results:
-             return results[0]['geometry']['lat'],results[0]['geometry']['lng']
+             lat = round (results[0]['geometry']['lat'],2)
+             lon = round (results[0]['geometry']['lng'],2)
+             return lat,lon
          else:
             return 'Город е найден.'
     except Exception as e:
         return  f'Возникла ошибка {e}'
 
 key = '82f3b2fd29a54869a709e582a85e84b7'
-city='London'
+city='Химки'
 coordinates = get_coordinates(city,key)
 print(f"Координаты города {city}: {coordinates}")
 
